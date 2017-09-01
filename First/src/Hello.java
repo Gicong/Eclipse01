6d5eefb7010e8479fcb7c6ef85681038eb4c60ac
浮生一梦 <Gicong@users.noreply.github.com>
2017-09-01 20:34:10 +0800

    Initial commit

---
+++ First/src/Hello.java
@@ -0,0 +1,23 @@
+
+public class Hello {
+	
+//	Ma
+//浮生一梦 <Gicong@users.noreply.github.com>浮生一梦
+//193ab86
+//18 changed files
+//一行代码
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("hello world");
+		
+		System.out.println("这就是GitHub");
+		
+		
+
+	}
+
+}
